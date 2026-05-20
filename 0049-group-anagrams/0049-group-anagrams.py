d240f5fef23f0f0004af93022873ac44aaddbef4
VarunRawatCodes05 <varun.rawat.cs28@iilm.edu>
2026-05-20 15:32:43 +0530

    Time: 6 ms (98.96%), Space: 22 MB (80.21%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        dic = collections.defaultdict(list)
+        for s in strs: 
+            dic["".join(sorted(s))].append(s)
+        return list(dic.values())
+        
