373bce3405afdc21fb7ebad0862b10c1587dc471
Varun Rawat <varun.rawat.cs28@iilm.edu>
2026-09-02 16:02:58 +0530

    Time: 2 ms (61.51%), Space: 44 MB (14.79%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.java
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -1,25 +1,37 @@
 class Solution {
-  public boolean buddyStrings(String s, String goal) {
-    if (s.length() != goal.length())
-      return false;
-    if (s.equals(goal) && hasDuplicateLetters(s))
-      return true;
+    public boolean buddyStrings(String s, String goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        if(s.equals(goal)){
+            
+            int [] arr = new int [26];
 
-    List<Integer> diffIndices = new ArrayList<>();
-
-    for (int i = 0; i < s.length(); ++i)
-      if (s.charAt(i) != goal.charAt(i))
-        diffIndices.add(i);
-
-    return diffIndices.size() == 2 &&
-        s.charAt(diffIndices.get(0)) == goal.charAt(diffIndices.get(1)) &&
-        s.charAt(diffIndices.get(1)) == goal.charAt(diffIndices.get(0));
-  }
-
-  private boolean hasDuplicateLetters(String s) {
-    int[] count = new int[26];
-    for (char c : s.toCharArray())
-      ++count[c - 'a'];
-    return Arrays.stream(count).anyMatch(freq -> freq > 1);
-  }
+            for(int i = 0 ; i < s.length() ; i++){
+            arr[s.charAt(i) - 'a']++;
+            if(arr[s.charAt(i) - 'a']>1){
+                return true;
+            }    
+        }
+        return false;
+        }
+        int DiffCnt = 0;
+        int FirstDiff = -1;
+        int SecondDiff = -1;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i)!=goal.charAt(i)){
+                DiffCnt++;
+                if(DiffCnt==1){
+                FirstDiff = i;
+            }
+                if(DiffCnt==2){
+                SecondDiff= i;
+            }
+            }
+        }
+        if(DiffCnt!=2){
+            return false;
+        }
+        return s.charAt(FirstDiff)==goal.charAt(SecondDiff) && s.charAt(SecondDiff)==goal.charAt(FirstDiff);
+    }
 }
