7a0568d28b189de978ca630fe22cebf3adfea4cb
Varun Rawat <varun.rawat.cs28@iilm.edu>
2026-09-21 19:05:57 +0530

    Time: 1 ms (99.93%), Space: 45.4 MB (49.37%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int compress(char[] chars) {
+
+        int index = 0;
+
+        for (int i = 0; i < chars.length; ) {
+
+            char ch = chars[i];
+            int count = 0;
+
+            while (i < chars.length && chars[i] == ch) {
+                i++;
+                count++;
+            }
+
+            chars[index++] = ch;
+
+            if (count > 1) {
+                String s = String.valueOf(count);
+
+                for (char c : s.toCharArray())
+                    chars[index++] = c;
+            }
+        }
+
+        return index;
+    }
+}
